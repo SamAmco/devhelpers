7d5986a18281bbf1bf16034c02dfd29b952032fd
SamAmco <samuel.amco@gmail.com>
2026-03-07 13:13:09 +0000

    wezterm resurrect setup

--- wezterm.lua
+++ wezterm.lua
@@ -1,15 +1,15 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
 local config = {}
 
 config.automatically_reload_config = true
 config.enable_kitty_keyboard = true
 config.font = wezterm.font 'JetBrains Mono'
 config.color_scheme = 'Tokyo Night Moon'
--- config.color_scheme = 'Batman'
--- config.color_scheme = 'Catppuccin Macchiato'
+config.window_close_confirmation = 'NeverPrompt'
 
 config.keys = {
-  { key = 'Backspace', mods = 'ALT', action = wezterm.action.SendKey { key = 'w', mods = 'CTRL' } },
+  { key = 'Backspace', mods = 'ALT',  action = wezterm.action.SendKey { key = 'w', mods = 'CTRL' } },
   { key = 'Backspace', mods = 'CTRL', action = wezterm.action.SendString '\x15' },
   {
     key = '%',
@@ -22,14 +22,30 @@ config.keys = {
     action = wezterm.action.SplitVertical {},
   },
   { key = 's', mods = 'ALT', action = wezterm.action.ShowLauncherArgs { flags = 'WORKSPACES' } },
-  { key = 's', mods = 'CTRL|SHIFT', action = wezterm.action.PromptInputLine {
-    description = 'Enter new workspace name',
-    action = wezterm.action_callback(function(window, pane, line)
-      if line then
-        window:perform_action(wezterm.action.SwitchToWorkspace { name = line }, pane)
-      end
-    end),
-  }},
+  {
+    key = 's',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.PromptInputLine {
+      description = 'Enter new workspace name',
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:perform_action(wezterm.action.SwitchToWorkspace { name = line }, pane)
+        end
+      end),
+    }
+  },
+  {
+    key = 'R',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.PromptInputLine {
+      description = 'Rename workspace',
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line)
+        end
+      end),
+    }
+  },
   { key = '1', mods = 'ALT', action = wezterm.action.ActivateTab(0) },
   { key = '2', mods = 'ALT', action = wezterm.action.ActivateTab(1) },
   { key = '3', mods = 'ALT', action = wezterm.action.ActivateTab(2) },
@@ -39,20 +55,55 @@ config.keys = {
   { key = '7', mods = 'ALT', action = wezterm.action.ActivateTab(6) },
   { key = '8', mods = 'ALT', action = wezterm.action.ActivateTab(7) },
   { key = '9', mods = 'ALT', action = wezterm.action.ActivateTab(8) },
-  { key = 'h', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Left' },
-  { key = 'l', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Right' },
-  { key = 'k', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Up' },
-  { key = 'j', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Down' },
+  { key = '[', mods = 'ALT', action = wezterm.action.ActivateTabRelative(-1) },
+  { key = ']', mods = 'ALT', action = wezterm.action.ActivateTabRelative(1) },
   { key = 'h', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Left' },
   { key = 'l', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Right' },
   { key = 'k', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Up' },
   { key = 'j', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Down' },
+
+  -- save all workspaces
+  {
+    key = 'w',
+    mods = 'ALT',
+    action = wezterm.action_callback(function(win, pane)
+      resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
+    end),
+  },
+  -- restore workspaces
+  {
+    key = "r",
+    mods = "ALT",
+    action = wezterm.action_callback(function(win, pane)
+      resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id, label)
+        local type = string.match(id, "^([^/]+)") -- match before '/'
+        id = string.match(id, "([^/]+)$")         -- match after '/'
+        id = string.match(id, "(.+)%..+$")        -- remove file extention
+        local opts = {
+          window = pane:window(),
+          close_open_tabs = true,
+          resize_window = false,
+          spawn_in_workspace = true,
+        }
+        if type == "workspace" then
+          local state = resurrect.state_manager.load_state(id, "workspace")
+          resurrect.workspace_state.restore_workspace(state, opts)
+        elseif type == "window" then
+          local state = resurrect.state_manager.load_state(id, "window")
+          resurrect.window_state.restore_window(pane:window(), state, opts)
+        elseif type == "tab" then
+          local state = resurrect.state_manager.load_state(id, "tab")
+          resurrect.tab_state.restore_tab(pane:tab(), state, opts)
+        end
+      end)
+    end),
+  },
 }
 
-local mux = wezterm.mux
 wezterm.on("gui-startup", function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
+  local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
   window:gui_window():maximize()
 end)
+
 
 return config
